abc7769fe3c2db721a6a2f9fe8255b3b394e01b4
Fabrício Gomes dos Santos <fabricio.gsantos16@gmail.com>
2024-04-10 21:01:58 -0300

    calculadora

--- aula 1/script.js
+++ aula 1/script.js
@@ -1,2 +1,48 @@
-console.log('hello world!!')
-alert('qual seu nome')
+function soma(){
+    var n1= parseFloat(document.getElementById('numero 1').value);
+    var n2= parseFloat(document.getElementById('numero 2').value);
+    var resultado= document.getElementById('resultado');
+
+    var soma= n1 + n2;
+    
+    resultado.innerText = soma;
+}
+
+function sub(){
+    var n1= parseFloat(document.getElementById('numero 1').value);
+    var n2= parseFloat(document.getElementById('numero 2').value);
+    var resultado= document.getElementById('resultado');
+
+    var sub = n1 - n2;
+
+    resultado.innerText= sub;
+}
+
+function mult(){
+    var n1= parseFloat(document.getElementById('numero 1').value);
+    var n2= parseFloat(document.getElementById('numero 2').value);
+    var resultado= document.getElementById('resultado');
+
+    var mult = n1 * n2;
+
+    resultado.innerText= mult;
+}
+
+function div(){
+    var n1= parseFloat(document.getElementById('numero 1').value);
+    var n2= parseFloat(document.getElementById('numero 2').value);
+    if (n2 === 0) {
+        document.getElementById('resultado').innerText="Erro: divisão por zero";
+    } else {
+        document.getElementById('resultado').innerText= n1 / n2;
+    }
+
+
+
+
+
+
+
+
+
+}
