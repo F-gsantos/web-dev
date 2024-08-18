6b260e2237ff05fba5cb9f4caafc6519c64de42b
Fabrício Gomes <fabricio.gsantos16@gmail.com>
2024-08-18 19:35:31 -0300

    vite-teste

---
+++ vite-project-test/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './componentes/Header'
+// IMPORTAÇÃO IMPORTANTE
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
+
+export default App
+// EXPORTAÇÃO DO COMPONENTE
